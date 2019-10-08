8c111fcbc8ab50aab5975a2dd083df8b7c1cfa0e
tianyanqiu <745916729@qq.com>
2019-10-08 16:53:46 +0800

    chore(docs): demo样式调整

--- docs/TabContentDemo.tsx
+++ docs/TabContentDemo.tsx
@@ -6,33 +6,26 @@ import TabPanel from '../src/components/TabPanel';
 export default function TabContentDemo() {
   const [active, setActive] = useState(0);
   const handlePrevClick = useCallback(() => {
-    if (active !== 0) {
-      setActive(active - 1);
-    } else {
-      setActive(0);
+    if (active === 0) {
+      return;
     }
+
+    setActive(active - 1);
   }, [active]);
 
   const handleNextClick = useCallback(() => {
-    if (active !== 2) {
-      setActive(active + 1);
-    } else {
-      setActive(2);
+    if (active === 2) {
+      return;
     }
+    setActive(active + 1);
   }, [active]);
 
   return (
     <>
       <TabContent active={active}>
-        <TabPanel id={0} style={{ background: 'red' }}>
-          内容1
-        </TabPanel>
-        <TabPanel id={1} style={{ background: 'yellow' }}>
-          内容2
-        </TabPanel>
-        <TabPanel id={2} style={{ background: 'blue' }}>
-          内容3
-        </TabPanel>
+        <TabPanel id={0}>内容1</TabPanel>
+        <TabPanel id={1}>内容2</TabPanel>
+        <TabPanel id={2}>内容3</TabPanel>
       </TabContent>
       <Button onClick={handlePrevClick}>上一页</Button>
       <Button onClick={handleNextClick}>下一页</Button>
