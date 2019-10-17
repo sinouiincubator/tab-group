2f1f4a771a086a39656923db417928da65a7a3e1
tianyanqiu <745916729@qq.com>
2019-10-17 19:27:36 +0800

    test(tab-header-item): 添加方向键切换的单元测试

--- src/components/tab-header-item/__tests__/TabHeaderItem.test.tsx
+++ src/components/tab-header-item/__tests__/TabHeaderItem.test.tsx
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { ThemeProvider } from 'styled-components';
 import defaultTheme from 'sinoui-components/styles/defaultTheme';
 import React from 'react';
+import useTabList from 'src/components/commons/useTabList';
 import TabHeaderContext from '../../TabHeaderContext';
 import TabHeaderItem from '../TabHeaderItem';
 import TabListContext from '../../TabListContext';
@@ -91,4 +92,41 @@ it('不在TabHeader中使用，不渲染TabHeaderItem', () => {
   );
 
   expect(container).toBeEmpty();
+});
+
+it('方向键切换标签页', () => {
+  const context = {
+    onSelect: jest.fn(),
+  };
+  function Demo() {
+    const tabListContext = useTabList(0);
+
+    return (
+      <ThemeProvider theme={defaultTheme}>
+        <TabListContext.Provider value={tabListContext}>
+          <TabHeaderContext.Provider value={context}>
+            <TabHeaderItem title="标签1" data-testid="tab1" />
+            <TabHeaderItem title="标签1" data-testid="tab2" disabled />
+            <TabHeaderItem title="标签1" data-testid="tab3" />
+          </TabHeaderContext.Provider>
+        </TabListContext.Provider>
+      </ThemeProvider>
+    );
+  }
+
+  const { getByTestId } = render(<Demo />);
+  act(() => {
+    fireEvent.click(getByTestId('tab1'));
+  });
+  fireEvent.keyDown(getByTestId('tab1'), { keyCode: 37 });
+
+  expect(context.onSelect).toHaveBeenCalledTimes(2);
+
+  fireEvent.keyDown(getByTestId('tab1'), { keyCode: 50 });
+
+  expect(context.onSelect).toHaveBeenCalledTimes(2);
+
+  fireEvent.keyDown(getByTestId('tab2'), { keyCode: 39 });
+
+  expect(context.onSelect).toHaveBeenCalledTimes(2);
 });
