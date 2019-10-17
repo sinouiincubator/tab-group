5e2e5b0675d0ebbac1f66b57c3a607550aa9a060
tianyanqiu <745916729@qq.com>
2019-10-17 16:34:57 +0800

    chore(tab-content): 高度根据内容自适应

--- src/components/tab-content/TabPanelListWrapper.tsx
+++ src/components/tab-content/TabPanelListWrapper.tsx
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 const TabPanelListWrapper = styled.div`
   display: flex;
   transition: transform 0.35s cubic-bezier(0.15, 0.3, 0.25, 1) 0s;
+  will-change: transform;
+
+  & > .sinoui-tab-panel-hidden {
+    height: 0px;
+  }
 `;
 
 export default TabPanelListWrapper;
--- src/components/tab-panel/TabPanelWrapper.tsx
+++ src/components/tab-panel/TabPanelWrapper.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const TabPanelWrapper = styled.div`
   width: 100%;
   flex-shrink: 0;
+  overflow: auto;
 `;
 
 export default TabPanelWrapper;
