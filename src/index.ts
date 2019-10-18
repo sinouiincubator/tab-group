dc05e84588e8420c0ebf2bd2196c757d30f98068
刘进行 <jackingstarliu@gmail.com>
2019-10-18 14:51:48 +0800

    chore: 导出 reset 方法

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import TabContent from './components/tab-content';
 import TabPanel from './components/tab-panel';
 import TabGroup from './components/TabGroup';
 import Tab from './components/tab';
+import { reset } from './helpers/uuid';
 
-export { Tab, TabHeader, TabHeaderItem, TabContent, TabPanel };
+export { Tab, TabHeader, TabHeaderItem, TabContent, TabPanel, reset };
 
 export default TabGroup;
