3ab010b449c16485ad24f77b5e0ed852408769aa
tianyanqiu <745916729@qq.com>
2019-10-23 18:15:40 +0800

    test(tab-panel): 添加标签页切换按钮的单元测试

--- src/components/commons/__tests__/mockTabListContext.ts
+++ src/components/commons/__tests__/mockTabListContext.ts
@@ -2,7 +2,7 @@ function mockTabListContext(selectedIndex = 0) {
   const register = jest.fn();
   const unregister = jest.fn();
   const getTabs = jest.fn();
-  const getTabProps = jest.fn();
+  const getTabProps = jest.fn().mockResolvedValue({ disabled: false });
 
   getTabs.mockReturnValue(['tab-0', 'tab-1', 'tab-2', 'tab-3']);
 
--- src/components/tab-panel/__tests__/TabPanel.test.tsx
+++ src/components/tab-panel/__tests__/TabPanel.test.tsx
@@ -1,11 +1,18 @@
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from 'sinoui-components/styles/defaultTheme';
 import '@testing-library/jest-dom/extend-expect';
+import Button from 'sinoui-components/Button';
+import TabContentContext from '../../TabContentContext';
 import TabPanel from '../TabPanel';
 import TabListContext from '../../TabListContext';
 import mockTabListContext from '../../commons/__tests__/mockTabListContext';
+import useTabState from '../../commons/useTabState';
+import { reset } from '../../../helpers/uuid';
 
 afterEach(cleanup);
+afterEach(reset);
 
 it('标签页不是当前标签页，则不渲染标签页内容', () => {
   const tabListContext = mockTabListContext();
@@ -135,4 +142,104 @@ it('不可见的标签面板内容不参与到重绘中', () => {
   );
 
   expect(contentRenderCount).toBe(2);
+});
+
+it('第一个标签内容中只显示下一页的切换按钮', () => {
+  function PrevButton() {
+    const { isFirst, prev } = useTabState();
+    return isFirst ? null : (
+      <Button data-testid="prevbutton" onClick={prev}>
+        上一页
+      </Button>
+    );
+  }
+
+  function NextButton() {
+    const { isLast, next } = useTabState();
+    return isLast ? null : (
+      <Button data-testid="nextbutton" onClick={next}>
+        下一页
+      </Button>
+    );
+  }
+
+  const tabListContext = mockTabListContext();
+  tabListContext.register.mockReturnValue(0);
+  tabListContext.getTabs.mockReturnValue(['tab-0', 'tab-1']);
+
+  const tabContentContext = {
+    onSelect: jest.fn(),
+    inTabContent: true,
+  };
+
+  const { getByTestId, queryByTestId } = render(
+    <ThemeProvider theme={defaultTheme}>
+      <TabListContext.Provider value={tabListContext}>
+        <TabContentContext.Provider value={tabContentContext}>
+          <TabPanel data-testid="tabpanel">
+            内容一
+            <PrevButton />
+            <NextButton />
+          </TabPanel>
+        </TabContentContext.Provider>
+      </TabListContext.Provider>
+    </ThemeProvider>,
+  );
+
+  expect(queryByTestId('prevbutton')).toBeNull();
+  expect(getByTestId('nextbutton')).toBeInTheDocument();
+
+  fireEvent.click(getByTestId('nextbutton'));
+
+  expect(tabContentContext.onSelect).toHaveBeenCalled();
+});
+
+it('最后一个标签页中只显示上一页的切换按钮', () => {
+  function PrevButton() {
+    const { isFirst, prev } = useTabState();
+    return isFirst ? null : (
+      <Button data-testid="prevbutton" onClick={prev}>
+        上一页
+      </Button>
+    );
+  }
+
+  function NextButton() {
+    const { isLast, next } = useTabState();
+    return isLast ? null : (
+      <Button data-testid="nextbutton" onClick={next}>
+        下一页
+      </Button>
+    );
+  }
+
+  const tabListContext = mockTabListContext();
+  tabListContext.register.mockReturnValue(0);
+  tabListContext.getTabs.mockReturnValue(['tab-1', 'tab-0']);
+
+  const tabContentContext = {
+    onSelect: jest.fn(),
+    inTabContent: true,
+  };
+
+  const { getByTestId, queryByTestId } = render(
+    <ThemeProvider theme={defaultTheme}>
+      <TabListContext.Provider value={tabListContext}>
+        <TabContentContext.Provider value={tabContentContext}>
+          <TabPanel data-testid="tabpanel" forceRenderContent>
+            内容一
+            <PrevButton />
+            <NextButton />
+          </TabPanel>
+        </TabContentContext.Provider>
+      </TabListContext.Provider>
+    </ThemeProvider>,
+  );
+
+  expect(getByTestId('prevbutton')).toBeInTheDocument();
+  expect(queryByTestId('nextbutton')).toBeNull();
+
+  fireEvent.click(getByTestId('prevbutton'));
+
+  expect(tabContentContext.onSelect).toHaveBeenCalled();
 });
